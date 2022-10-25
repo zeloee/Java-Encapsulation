436b756d724d377520e8ab1f1abcedd0cf9dc08f
BernardSapida <BernardSapida1706@gmail.com>
2022-10-25 22:23:34 +0800

    Encapsulation (done)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,68 @@
+import java.util.Scanner;
+
+// Identity
 public class Main {
+    // State
+    public Scanner sc = new Scanner(System.in);
+    public String displayOptions = 
+        "[1] iPhone 14 - Php 61,990\n" + 
+        "[2] iPhone 14 Plus - Php 68,990\n" +
+        "[3] iPhone 14 Pro - Php 76,990\n" +
+        "[4] iPhone 14 Pro Max - Php 83,990\n\n" +
+        "[1] Regular - No discount\n" +
+        "[2] Student - 10%\n" +
+        "[3] PWD - 20%\n" +
+        "[4] Senior Citizen - 30%\n";
+    public int selectedBrand, selectedDiscount;
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Noella!");
+        // Code Runner
+
+        // Instantiation of the class (Creating object)
+        Main main = new Main();
+        main.startTransaction();
+    }
+
+    // Behavior (One method/function should do one thing)
+    // Rule: method first word should be a verb
+    public void startTransaction() {
+        System.out.println(displayOptions);
+        queryBrand();
+        queryStatus();
+        displayComputation();
+    }
+
+    public void queryBrand() {
+        System.out.print("Enter your choice of brand [1-4]: ");
+        selectedBrand = sc.nextInt();
+    }
+
+    public void queryStatus() {
+        System.out.print("Enter status [1-4]: ");
+        selectedDiscount = sc.nextInt();
+    }
+
+    public void displayComputation() {
+        Child child = new Child();
+        child.setTotalBill(selectedBrand, selectedDiscount);
+        System.out.println("Your total bill is: " + child.getTotalBill());
     }
 }
+
+/*
+    [1] iPhone 14 - Php 61,990
+    [2] iPhone 14 Plus - Php 68,990
+    [3] iPhone 14 Pro - Php 76,990
+    [4] iPhone 14 Pro Max - Php 83,990
+
+    [1] Regular - No discount
+    [2] Student - 10%
+    [3] PWD - 20%
+    [4] Senior Citizen - 30%
+
+    Enter your choice of brand [1-4]: 1
+    Enter status [1-4]: 1
+    Total bill: 
+
+    4 * 4 = 16 IF's
+*/
