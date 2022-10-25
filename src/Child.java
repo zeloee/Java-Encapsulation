3bd5faa20407c01ffbdf63c2af50fa736e0de1ed
zeloee <mallowella17@gmail.com>
2022-10-25 22:32:32 +0800

    done na

--- src/Child.java
+++ src/Child.java
@@ -1,7 +1,15 @@
 public class Child {
-    String name = "Bernard Sapida";
+    
+    private int [] priceList = { 61990, 68990, 76990, 83990 };
+    private double[] discountList = { .0, .1, .2, .3};
+    private double totalBill;
 
-    public String getName() {
-        return name;
+    // Formula = price - (price * discount)
+    public void setTotalBill(int selectedBrand, int selectedDiscount) {
+        totalBill = priceList[selectedBrand-1] - (priceList[selectedBrand-1] * discountList[selectedDiscount-1]);
+    }
+
+    public double getTotalBill(){
+        return totalBill;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
+import java.util.Scanner;
+
 public class Main {
+    
+    public Scanner sc = new Scanner(System.in);
+    public String displayOptions = 
+    "[1] iPhone 14 - Php 61,990\n" +
+    "[2] iPhone 14 Plus - Php 68,990\n" +
+    "[3] iPhone 14 Pro - Php 76,990\n" +
+    "[4] iPhone 14 Pro Max - Php 83,990\n\n" +
+
+    "[1] Regular - No discount\n" +
+    "[2] Student - 10%\n" +
+    "[3] PWD - 20%\n" +
+    "[4] Senior Citizen - 30%\n";
+    public int selectedBrand, selectedDiscount;
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Noella Julia!");
+       
+
+        Main main = new Main();
+        main.startTransaction();
+    }
+    
+    public void startTransaction() {
+        System.out.println(displayOptions);
+        queryBrand();
+        queryStatus();
+        displayComputation();
+
+
+    }
+
+    public void queryBrand(){
+        System.out.print("Enter your choice of brand [1-4]: ");
+        selectedBrand = sc.nextInt();
+    }
+    public void queryStatus(){
+        System.out.print("Enter your choice of brand [1-4]: ");
+        selectedDiscount = sc.nextInt();
+
+}
+
+    public void displayComputation() {
+        Child child = new Child();
+        child.setTotalBill(selectedBrand, selectedDiscount);
+        System.out.println("Your total bill is: " + child.getTotalBill());
     }
 }
+
